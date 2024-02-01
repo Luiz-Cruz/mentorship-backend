2469607c71c827e605f18f912f9404b26674d31f
Luiz Cruz <luizcruzdev@gmail.com>
2024-01-31 21:06:01 -0300

    update user routes

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+
 const users = [
   {
+    id: 1,
     name: "Luiz",
     age: 23,
     height: 1.84,
     weight: 75
   },
   {
+    id: 2,
     name: "Akhenaton",
     age: 21,
     height: 1.80,
     weight: 69
   },
   {
+    id: 3,
     name: "Gabriela",
     age: 19,
     height: 1.64,
     weight: 61
   },
   {
+    id: 4,
     name: "Volmir",
     age: 41,
     height: 1.79,
@@ -31,27 +37,42 @@ const users = [
 
 // CREATE
 app.post('/users', (req,res) => {
-  res.send('Create something!')
+  // user para adicionar um usuario novo no array users
+  const newUser = req.body;
+  users.push(newUser);
+  res.status(201).json({"status": 201, "data": newUser, "message": "User created successfully"})
 });
 
-// READ
+// READ ALL
 app.get('/users', (req, res) => {
+  // retornar o array users
+  res.status(200).json({"status": 200, "data": users})
+});
+
+// READ BY ID
+app.get('/users/:id', (req, res) => {
+  // retornar o usuário de acordo com o id 
+  const userId = req.params.id;
+  
   res.status(200).json({"status": 200, "data": users})
 });
 
 // UPDATE
-app.put('/users', (req,res) => {
+app.put('/users/:id', (req,res) => {
+  // atualizar completamente o usuario através do id
   res.send('Update an entire data!')
 });
 
 // UPDATE
-app.patch('/users', (req,res) => {
+app.patch('/users/:id', (req,res) => {
+  // atualizar parcialmente um usuario através do id
   res.send('Update partially some date!')
 });
 
 
 // DELETE
-app.delete('/users', (req,res) => {
+app.delete('/users/:id', (req,res) => {
+  // remover do array o usuário especificado pelo id
   res.send('Delete some data!')
 });
  
